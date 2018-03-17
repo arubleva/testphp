f8e493b154ff18e5ba7634b558d56456320bd49f
arubleva <34969454+arubleva@users.noreply.github.com>
2018-03-17 15:22:23 +0100

    Add files via upload

--- aendern.php
+++ aendern.php
@@ -31,8 +31,36 @@ if ($resultat->num_rows > 0){
 	while($zeile=$resultat->fetch_assoc()){
 		echo "<from action='aendern_durchfuehren.php' method='POST'>";
 		
-		echo "<Vorname:<br/>";
+		echo "Vorname:<br/>";
 		echo "<input type='text' name='Vorname' value = '".$zeile["Vorname"]."' /><br/>";
+		
+		echo "Nachname:<br/>";
+		echo "<input type='text' name='Nachname' value = '".$zeile["Nachname"]."' /><br/>";
+		
+		echo "Titel:<br/>";
+		echo "<input type='text' name='Titel' value = '".$zeile["Titel"]."' /><br/>";
+		
+		echo "Firma:<br/>";
+		echo "<input type='text' name='Firma' value = '".$zeile["Firma"]."' /><br/>";
+		
+		echo "Strasse:<br/>";
+		echo "<input type='text' name='Strasse' value = '".$zeile["Strasse"]."' /><br/>";
+		
+		echo "Ort:<br/>";
+		echo "<input type='text' name='Ort' value = '".$zeile["Ort"]."' /><br/>";
+		
+		echo "Bundesland:<br/>";
+		echo "<input type='text' name='Bundesland' value = '".$zeile["Bundesland"]."' /><br/>";
+		
+		echo "Postleitzahl:<br/>";
+		echo "<input type='text' name='Postleitzahl' value = '".$zeile["Postleitzahl"]."' /><br/>";
+		
+		echo "Land:<br/>";
+		echo "<input type='text' name='Land' value = '".$zeile["Land"]."' /><br/>";
+		
+		echo "<input type='hidden' name='Zaehler' value='".$zeile["Zaehler"]."' />";
+		
+		echo "<input type='submit' value='Ändern' />";
 		
 		echo "</form>";
 		
